841bb7e0fe5d009451d3122784d472a9b2d926e7
Tyler Hawkins <thawkins@youniqueproducts.com>
2018-08-26 20:02:48 -0600

    fixed route change error; added gitignore; minor styleguide updates

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -4,29 +4,28 @@ app.config(function ($routeProvider) {
 	$routeProvider
 		.when('/', {
 			redirectTo: '/main',
-			access: {restricted: false}
+			access: { restricted: false }
 		})
 		.when('/main', {
 			controller: 'mainController',
 			templateUrl:'javascripts/app/views/main.html',
-			access: {restricted: true}
+			access: { restricted: true }
 		})
 		.when('/enterChatroom', {
 			controller: 'enterChatroomController',
 			templateUrl:'javascripts/app/views/enterChatroom.html',
-			access: {restricted: false}
+			access: { restricted: false }
 		})
 		.otherwise({ 
 			redirectTo: '/main',
-			access: {restricted: false}
+			access: { restricted: false }
 		});
 });
 
 app.run(function($rootScope, $location, $window, $route, socket) {
-
     // AUTHENTICATION
 	$rootScope.$on('$routeChangeStart', function (event, next, current) {
-	    if (typeof next.access != 'undefined' && next.access.restricted && !$rootScope.fields.username) {
+	    if (typeof next.access !== 'undefined' && next.access.restricted && !$rootScope.fields.username) {
 			$location.path('/enterChatroom');
 			$route.reload();
 		} 
@@ -34,21 +33,22 @@ app.run(function($rootScope, $location, $window, $route, socket) {
 
 	// SOCKET.IO EVENT FOR LEAVING CHATROOM
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-		if (typeof current != "undefined" 
-			&& typeof previous != "undefined" 
-			&& current.loadedTemplateUrl.indexOf("enterChatroom.html") != -1
-			&& previous.loadedTemplateUrl.indexOf("main.html") != -1) {
+		if (typeof current !== 'undefined'
+			&& current.loadedTemplateUrl
+			&& typeof previous !== 'undefined' 
+			&& previous.loadedTemplateUrl
+			&& current.loadedTemplateUrl.indexOf('enterChatroom.html') != -1
+			&& previous.loadedTemplateUrl.indexOf('main.html') != -1) {
 			socket.emit('userLeftChatroom', {
-				username: "Chatroom",
-				message: $rootScope.fields.username + " has left the chatroom",
-				nameColor: "#606060"
+				username: 'Chatroom',
+				message: $rootScope.fields.username + ' has left the chatroom',
+				nameColor: '#606060',
 			});
 		}
     });
 
     // ROOT SCOPE FIELDS
     $rootScope.fields = {
-    	username: ""
+    	username: '',
     };
-
 });
